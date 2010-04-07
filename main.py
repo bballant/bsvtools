41c63571d224f7d2b7de750e1e676b566217a78d
Brian Ballantine <brian@sawfly.(none)>
2010-04-07 17:38:09 -0400

    fixed ordering of cams when creating composite

--- main.py
+++ main.py
@@ -16,13 +16,15 @@ SQL_FILE    = "/tmp/images.sqlite3"
 FONT_FILE   = "FreeMonoBold.ttf"
 
 if __name__ == '__main__':
-    cams = common.list_cams(common.CAMS)
-    ftable = common.create_frametable(BASE_DIR, cams)
     if len(sys.argv) == 3 and sys.argv[1] == "composite":
+        cams = composite.order_for_composite(common.CAMS)
+        ftable = common.create_frametable(BASE_DIR, cams)
         common.create_dir(sys.argv[2])
         composite.write_images(ftable, sys.argv[2], "960x720", 
                  len(cams), FONT_FILE) 
     else:
+        cams = common.list_cams(common.CAMS)
+        ftable = common.create_frametable(BASE_DIR, cams)
         taggedevents = gen_web.gpsevents(EVENT_FILE, True)
         tagged_hash = gen_web.taggedhash(ftable, taggedevents)
         gps_hash = gen_web.gpshash(ftable, gen_web.gpsevents(GPS_FILE))
